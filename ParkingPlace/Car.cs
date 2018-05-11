d213a01642c3141d52a700f821176a01642519de
Yakovliev Vladyslav <vladyakovliev1@gmail.com>
2018-05-11 21:24:50 +0300

    Add properties to Car class

--- ParkingPlace/Car.cs
+++ ParkingPlace/Car.cs
@@ -8,5 +8,19 @@ namespace ParkingPlace
 {
     public class Car
     {
+        /// <summary>
+        /// Identifier of car
+        /// </summary>
+        public int ID { get; set; }
+
+        /// <summary>
+        /// Car account balance
+        /// </summary>
+        public double Balance { get; set; }
+
+        /// <summary>
+        /// Car type from enum CarTypes
+        /// </summary>
+        public CarTypes CarType { get; set; }
     }
 }
