5ac8a11c4aa7d4a16e12e08c4ae1c7646a5e3bb4
Yakovliev Vladyslav <vladyakovliev1@gmail.com>
2018-05-13 21:08:52 +0300

    Property Id of Car class must be autoincrement

--- ParkingPlace/Car.cs
+++ ParkingPlace/Car.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ParkingPlace
 {
-    public class Car
+    public class Car : IDisposable
     {
         /// <summary>
         /// Identifier of car
@@ -27,5 +27,61 @@ namespace ParkingPlace
         /// Number of parking place
         /// </summary>
         public int NumberOfParkingPlace { get; set; }
+
+        /// <summary>
+        /// UsedCounter used for realizing Id property as autoincrement
+        /// </summary>
+        private static List<bool> UsedCounter = new List<bool>();
+
+        private static object Lock = new object();
+
+        public Car()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            lock (Lock)
+            {
+                UsedCounter[Id] = false;
+            }
+        }
+
+        /// <summary>
+        /// Get available index for Id property
+        /// </summary>
+        /// <returns>Available Id for instance of car</returns>
+        private int GetAvailableIndex()
+        {
+            for (int i = 0; i < UsedCounter.Count; i++)
+            {
+                if (UsedCounter[i] == false)
+                {
+                    return i;
+                }
+            }
+
+            // Nothing available.
+            return -1;
+        }
+
+        /// <summary>
+        /// Set Id of car
+        /// </summary>
+        private void SetIdOfCar()
+        {
+            lock (Lock)
+            {
+                int nextIndex = GetAvailableIndex();
+                if (nextIndex == -1)
+                {
+                    nextIndex = UsedCounter.Count;
+                    UsedCounter.Add(true);
+                }
+
+                Id = nextIndex;
+            }
+        }
     }
 }
