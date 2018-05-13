536d7967212eadd0176778712bb7d66390b92d7b
Yakovliev Vladyslav <vladyakovliev1@gmail.com>
2018-05-13 23:53:44 +0300

    AddCar method in Parking class

--- ParkingPlace/Parking.cs
+++ ParkingPlace/Parking.cs
@@ -11,7 +11,7 @@ namespace ParkingPlace
         /// <summary>
         /// List of cars
         /// </summary>
-        public List<Car> ListOfCars { get; set; }
+        public List<Car> ListOfCars { get; private set; } = new List<Car>();
 
         /// <summary>
         /// List of transaction
@@ -33,14 +33,107 @@ namespace ParkingPlace
         /// </summary>
         public Settings Settings { get; private set; }
 
+        /// <summary>
+        /// Free parking places list
+        /// </summary>
+        private static List<bool> FreeParkingPlacesList;
+
         private Parking()
         {
             Settings = Settings.GetSettings();
+
+            FreeParkingPlacesList = new List<bool>();
+
+            //All parking places are free when parking initializing
+            for (int i = 0; i < Settings.ParkingSpace; i++)
+            {
+                FreeParkingPlacesList.Add(true);
+            }
         }
 
         public static Parking GetParking()
         {
             return parking;
+        }
+
+        /// <summary>
+        /// If method return true than there is free parking place, 
+        /// if meghod return false than there are no free parking places.
+        /// </summary>
+        /// <returns>If method return true than there is free parking place.</returns>
+        private bool IsThereFreeParkingPlace()
+        {
+            bool freeParkingPlace = false;
+
+            foreach (bool item in FreeParkingPlacesList)
+            {
+                if (item == true)
+                {
+                    freeParkingPlace = true;
+                    break;
+                }
+            }
+
+            return freeParkingPlace;
+        }
+
+        /// <summary>
+        /// Add car on parking WITHOUT starting balance.
+        /// </summary>
+        /// <param name="carType">Type of car</param>
+        public void AddCar(CarTypes carType)
+        {
+            bool freeParkingPlace = IsThereFreeParkingPlace();
+
+            int numberOfFreeParkingPlace = 0;
+
+            if (freeParkingPlace)
+            {
+                for (int i = 0; i < FreeParkingPlacesList.Count; i++)
+                {
+                    if (FreeParkingPlacesList[i] == true)
+                    {
+                        numberOfFreeParkingPlace = i;
+                        break;
+                    }
+                }
+
+                ListOfCars.Add(new Car(carType, numberOfFreeParkingPlace));
+            }
+            else
+            {
+                throw new NoFreeParkingPlacesException("No free parking places!");
+            }
+        }
+
+        /// <summary>
+        /// Add car on parking WITH starting balance.
+        /// </summary>
+        /// <param name="carType">Type of car</param>
+        /// <param name="defaultBalance">Starting balance of car</param>
+        public void AddCar(CarTypes carType, double defaultBalance)
+        {
+            bool freeParkingPlace = IsThereFreeParkingPlace();
+
+            int numberOfFreeParkingPlace = 0;
+
+            if (freeParkingPlace)
+            {
+                for (int i = 0; i < FreeParkingPlacesList.Count; i++)
+                {
+                    if (FreeParkingPlacesList[i] == true)
+                    {
+                        numberOfFreeParkingPlace = i;
+                        break;
+                    }
+                }
+
+                ListOfCars.Add(new Car(carType, numberOfFreeParkingPlace, defaultBalance));
+            }
+            else
+            {
+                throw new NoFreeParkingPlacesException("No free parking places!");
+            }
         }
     }
 }
--- ParkingPlace/Program.cs
+++ ParkingPlace/Program.cs
@@ -10,6 +10,15 @@ namespace ParkingPlace
     {
         static void Main(string[] args)
         {
+            //Test
+            //Parking parking = Parking.GetParking();
+
+            //parking.AddCar(CarTypes.Bus);
+            //parking.AddCar(CarTypes.Motorcycle, 10);
+            //parking.AddCar(CarTypes.Passenger, 0);
+            //parking.AddCar(CarTypes.Motorcycle, -2);
+
+
         }
     }
 }
