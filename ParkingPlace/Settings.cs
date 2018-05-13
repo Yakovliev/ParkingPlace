776046e241d412fd17a5b7c909fabfbe460d8de2
Yakovliev Vladyslav <vladyakovliev1@gmail.com>
2018-05-13 19:48:54 +0300

    Added properties to Settings class

--- ParkingPlace/Settings.cs
+++ ParkingPlace/Settings.cs
@@ -8,5 +8,35 @@ namespace ParkingPlace
 {
     public class Settings
     {
+        /// <summary>
+        /// Timeout - is number of seconds. Every Timeout-seconds write-offs funds for a parking space.
+        /// </summary>
+        public int Timeout { get; private set; } = 3;
+
+        /// <summary>
+        /// Parking prices.
+        /// </summary>
+        private Dictionary<CarTypes, double> parkingPrices = new Dictionary<CarTypes, double>()
+        {
+            {CarTypes.Bus, 4D },
+            {CarTypes.Truck, 5D },
+            {CarTypes.Passenger, 3D },
+            {CarTypes.Motorcycle, 2D },
+        };
+
+        /// <summary>
+        /// Get parking prices.
+        /// </summary>
+        public Dictionary<CarTypes, double> ParkingPrices { get { return parkingPrices; } }
+
+        /// <summary>
+        /// Parking space.
+        /// </summary>
+        public int ParkingSpace { get; private set; } = 10;
+        
+        /// <summary>
+        /// Coefficient of fine
+        /// </summary>
+        public double Fine { get; private set; } = 2d;
     }
 }
