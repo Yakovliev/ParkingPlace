04a2e9ea47c4bd8595d8dc99faa35cf1fb03c170
Yakovliev Vladyslav <vladyakovliev1@gmail.com>
2018-05-14 01:07:10 +0300

    DeleteCar method in Parking class

--- ParkingPlace/Program.cs
+++ ParkingPlace/Program.cs
@@ -16,6 +16,14 @@ namespace ParkingPlace
             //parking.AddCar(CarTypes.Bus);
             //parking.AddCar(CarTypes.Motorcycle, 10);
             //parking.AddCar(CarTypes.Passenger, 0);
+
+            //parking.DeleteCarByNumberOfParkingPlace(1);
+            //parking.AddCar(CarTypes.Truck);
+            //parking.AddCar(CarTypes.Passenger, 20);
+
+            //parking.DeleteCarById(2);
+            //parking.AddCar(CarTypes.Bus, 20);
+
             //parking.AddCar(CarTypes.Motorcycle, -2);
 
 
